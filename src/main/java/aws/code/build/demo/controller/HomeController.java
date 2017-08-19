6e3ef2bc522a9e7db6a4517dc42af8d048ed5719
Vincent W M CHEN <443519782@qq.com>
2017-08-19 11:47:18 +0800

    initial commit

---
+++ src/main/java/aws/code/build/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package aws.code.build.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping({ "/", "/index" })
+	public String home() {
+		return "index " + new java.util.Date();
+	}
+
+}
