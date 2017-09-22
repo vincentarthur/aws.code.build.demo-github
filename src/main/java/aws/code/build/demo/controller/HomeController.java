a47609dabe6b18b75aec5d3135c8f29526395298
Vincent W M CHEN <443519782@qq.com>
2017-09-22 14:54:29 +0800

    version 15

--- src/main/java/aws/code/build/demo/controller/HomeController.java
+++ src/main/java/aws/code/build/demo/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
 	@RequestMapping({ "/", "/index" })
 	public String home() {
-		return "index " + new java.util.Date() + " Version 10";
+		return "index " + new java.util.Date() + " Version 15";
 	}
 
 }
